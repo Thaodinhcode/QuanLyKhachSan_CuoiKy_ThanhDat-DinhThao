460b0988626da880cf3f6b18d18defc0dfa4f2e4
DinhThao <dinhthao2xxx@gmail.com>
2026-05-03 21:59:15 +0700

    test commit lan 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
     <section>
-      <h1>UserContext</h1>
-      <p>Đây là phần hiển thị thông tin người dùng.</p>
+      <h1>Test thử</h1>
+      <p></p>
     </section>
   );
 }
--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,7 +1,7 @@
 export default function UserContext() {
   return (
     <section>
-      <h1>UserContext</h1>
+      <h1>test thử</h1>
       <p>Đây là phần hiển thị thông tin người dùng.</p>
     </section>
   );
